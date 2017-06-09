d769dc5fcbababb5b6ea9b6f13b24ab294f8eb10
Leo <leonoragilmour@sky.com>
2017-06-09 11:23:08 +0100

    init

---
+++ agreable-feature-box-widget-plugin.php
@@ -0,0 +1,22 @@
+<?php
+/**
+* @wordpress-plugin
+* Plugin Name: Agreable Feature Box Widget Plugin
+* Plugin URI: http://github.com/shortlist-digital/agreable-feature-box-widget-plugin
+* Description: A plugin for the feature box widget
+* Version: 1.0.0
+* Author: Shortlist Studio
+* Author URI: http://shortlist.studio
+* License: MIT
+*/
+
+require_once __DIR__ . '/../../../../vendor/autoload.php';
+
+class AgreableFeatureBoxWidgetPlugin
+{
+    public function __construct()
+    {
+        //
+    }
+}
+new AgreableFeatureBoxWidgetPlugin();
---
+++ readme.md
@@ -0,0 +1,2 @@
+# Agreable Feature Box Widget Plugin
+A plugin for the feature box widget
